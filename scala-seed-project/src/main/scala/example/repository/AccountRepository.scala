3f251f0b468afb5b39230d2389deb27469bfa8ee
Henning Koller <henkol@knowit.no>
2018-10-11 13:28:41 +0200

    AccountRepo first iteration.

--- scala-seed-project/src/main/scala/example/repository/AccountRepository.scala
+++ scala-seed-project/src/main/scala/example/repository/AccountRepository.scala
@@ -1,8 +1,27 @@
 package example.repository
 
+import java.util.UUID
+
 import example.models.{Account, AccountType, Customer}
 
 class AccountRepository {
-  def create(customer: Customer, accountType: AccountType, balance: Double): Account = Account(None, accountType, balance)
-  def get(id: String): Option[Account] = ???
+
+  var allAccounts: Map[String, List[Account]] = Map.empty
+
+  def create(customer: Customer, accountType: AccountType, balance: Double): Account = {
+    val customerId = customer.id.getOrElse(throw new IllegalArgumentException("We need a customer with an ID to create an account!"))
+
+    val newAccount = Account(Option(UUID.randomUUID().toString), accountType, balance)
+    val customerAccounts = allAccounts.getOrElse(customerId, List.empty[Account]) :+ newAccount
+    allAccounts(customer.id.get) = customerAccounts
+    newAccount
+  }
+
+  def getAccountsForCustomer(customerId: String): List[Account] = {
+    allAccounts.getOrElse(customerId, List.empty[Account])
+  }
+
+  def get(id: String): Option[Account] = {
+    allAccounts.values.flatten.find(_.id.contains(id))
+  }
 }
